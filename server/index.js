d9f20f92829e93d3357a0c8ca3c29f2c042816eb
pawan wadhwani <pawanw17@gmail.com>
2019-01-26 22:53:55 +0530

    more changes

--- server/index.js
+++ server/index.js
@@ -3,9 +3,16 @@ var s = new server({port: 5001});
 
 
 s.on('connection',function(ws){
-	// ws.on('message',function(message){
-	// 	console.log("Received: "+message);
-	var obj = {lat:25.5941, lng:85.1376};
-		ws.send(JSON.stringify(obj));
-	// });
+	 ws.on('message',function(message){
+	 	console.log("Received: "+ message);
+	// var obj = {lat:25.5941, lng:85.1376};
+		s.clients.forEach(function e(client){
+			if(client==ws)
+				client.send(message);
+		});
+		ws.on('close',function(){
+			console.log("I lost a client");
+		});
+		//ws.send(JSON.stringify(obj));
+	 });
 });
