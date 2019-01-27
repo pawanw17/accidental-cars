4cee5424896593b20cf2f273c60cc8801d3fb91f
pawan wadhwani <pawanw17@gmail.com>
2019-01-27 21:19:41 +0530

    some more changes

--- application.py
+++ application.py
@@ -27,6 +27,8 @@ app = Flask(__name__)
 def index():
 	"""Render map"""
 	return render_template('index.html')
+# if __name__=='__main__':
+	# app.run(host='0.0.0.0')
 
 # @socketio.on('message')
 # def handle_message(message):
